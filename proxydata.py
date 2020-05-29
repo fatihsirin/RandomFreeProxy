eb4b86225433abaff887b2f03facd7c0c4c9dd5a
nope <fatihsirin0@gmail.com>
2020-05-29 15:21:30 +0300

    .

--- proxydata.py
+++ proxydata.py
@@ -1,64 +1,14 @@
-import asyncio
 import re
 import time
-
-import aiohttp
+import os
 import requests
+from datetime import datetime
+import os
+import re
+import time
+from datetime import datetime
 
-
-#
-# def regProxy():
-#     # pattern_proxy = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$"
-#     pattern_proxy = r"([0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{2,4})"
-#     compiledReg = re.compile(pattern_proxy, re.MULTILINE)
-#     return compiledReg
-#
-#
-# def extract(data, patt):
-#     result = []
-#     while True:
-#         m = patt.search(data)
-#         if not m:
-#             break
-#         i = m.group()
-#         if not i in result:
-#             result.append(m.group())
-#         data = data[m.end():]
-#     return result
-#
-#
-# def dataUrl():
-#     sources = None
-#     with open(r'./sources.txt', 'r') as f:
-#         sources = f.read().splitlines()
-#     return sources
-#
-#
-# def getData():
-#     IpList = []
-#     proxyFinder = regProxy()
-#     urls = dataUrl()
-#     for url in urls:
-#         headers = {
-#             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-#             'Accept': "application/json, text/javascript, */*; q=0.01",
-#             'Accept-Language': "de,en-US;q=0.7,en;q=0.3",
-#             'X-Requested-With': "XMLHttpRequest",
-#             'Referer': url,
-#             # 'Connection':"keep-alive"
-#         }
-#         response = requests.get(url, headers=headers)
-#         time.sleep(1)
-#         statuscode = response.status_code
-#         if statuscode == 200:
-#             data = extract(response.text, proxyFinder)
-#             IpList = IpList + data
-#
-#     IpList = list(dict.fromkeys(IpList))
-#     return IpList
-#
-
-# getData()
+import requests
 
 
 class Scrapper:
@@ -74,21 +24,42 @@ class Scrapper:
         """
         # init with Empty Proxy List
         self.proxies = []
-        self.sources = self.dataUrl()
+        self.sources = None
+        self.directorypath = "./ProxyData/"
+        self.file_ips = 'IpList.txt'
+        self.success_ips = 'Success.txt'
+        self.failed_ips = 'Failed.txt'
 
-    def dataUrl(self):
+    def init(self):
+        print('[+]started')
+        if not os.path.exists(self.directorypath + self.file_ips):
+            self.sources = self.DataUrls()
+            data = self.GetData(urls=self.sources)
+            self.WriteFile(filename=self.file_ips, data=data)
+            return data
+        else:
+            if self.LastModifTimeDifFile(self.directorypath + self.file_ips) > 120:
+                self.sources = self.DataUrls()
+                data = self.GetData(urls=self.sources)
+                self.WriteFile(data)
+            else:
+                data = self.ReadFile(filename=self.file_ips)
+                return data
+        print('[+]done')
+
+    def DataUrls(self):
         sources = None
         with open(r'./sources.txt', 'r') as f:
             sources = f.read().splitlines()
         return sources
 
-    def regProxy(self):
+    def RegProxy(self):
         # pattern_proxy = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$"
         pattern_proxy = r"([0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{2,5})"
         compiledReg = re.compile(pattern_proxy, re.MULTILINE)
         return compiledReg
 
-    def extract(self, data, patt):
+    def Extract(self, data, patt):
         result = []
         while True:
             m = patt.search(data)
@@ -103,10 +74,30 @@ class Scrapper:
     def pprint(self):
         print(self.sources)
 
-    def getData(self):
+    def WriteFile(self, filename, data):
+        if not os.path.exists(self.directorypath):
+            os.makedirs(self.directorypath)
+        with open(self.directorypath + filename, 'w') as f:
+            f.writelines("%s\n" % l for l in data)
+
+    def ReadFile(self, filename):
+        if not os.path.exists(self.directorypath):
+            return False
+        else:
+            with open(self.directorypath + filename, 'r') as f:
+                readed_data = f.readlines()
+                return readed_data
+
+    def LastModifTimeDifFile(self, file):
+        lastmodif = datetime.strptime(time.ctime(os.path.getmtime(file)), "%a %b %d %H:%M:%S %Y")
+        now = datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
+        dif = int((now - lastmodif).total_seconds() / 60.0)
+        print('time dif', str(dif))
+        return dif
+
+    def GetData(self, urls):
         IpList = []
-        proxyFinder = self.regProxy()
-        urls = self.sources
+        proxyFinder = self.RegProxy()
         for url in urls:
             print("[+] Started to Download: " + url)
             headers = {
@@ -122,7 +113,7 @@ class Scrapper:
             statuscode = response.status_code
             if statuscode == 200:
                 print("[+] Successfully Downloaded")
-                data = self.extract(response.text, proxyFinder)
+                data = self.Extract(response.text, proxyFinder)
                 IpList = IpList + data
             else:
                 print("[-] Got Problem with " + url)
@@ -131,74 +122,12 @@ class Scrapper:
         return IpList
 
 
-TIMEOUT = 30
-
-
-CHECK_URLS = (
-    ('ifconfigco', 'https://ifconfig.co/ip'),
-    ('ifconfigme', 'http://ifconfig.me/ip'),
-    ('ipinfo', 'http://ipinfo.io/ip'),
-)
-
-
-async def check_proxy(proxy):
-    """Try to load content of several commonly known websites through proxy"""
-
-    timeout = aiohttp.ClientTimeout(total=TIMEOUT)
-    result = dict()
-    ip, port = proxy.split(':')
-    result['ip'] = ip
-    result['port'] = port
-
-    for website_name, url in CHECK_URLS:
-        try:
-            async with aiohttp.ClientSession(timeout=timeout, connector=aiohttp.TCPConnector(ssl=False)) as session:
-                start = time.time()
-                status_code = 404
-                total_time = None
-                error_msg = 'no'
-                await asyncio.sleep(5)
-                async with session.get(url, proxy='http://{}:{}'.format(ip, port)) as resp:
-                    await asyncio.sleep(5)
-                    status_code = resp.status
-                    await resp.text()
-                    end = time.time()
-                    total_time = int(round(end - start, 2) * 1000)
-        except asyncio.TimeoutError:
-            status_code = 408
-            error_msg = 'timeout error'
-        except aiohttp.client_exceptions.ClientProxyConnectionError:
-            error_msg = 'connection error'
-            status_code = 503
-        except Exception as e:
-            status_code = 503
-            error_msg = 'unknown error: {}.'.format(e)
-        finally:
-            await asyncio.sleep(5)
-            result[website_name + '_status'] = status_code
-            result[website_name + '_error'] = error_msg
-            result[website_name + '_total_time'] = total_time
-            print(result)
-
-    return result
-
-
-async def runner(complete_list):
-    tasks = [check_proxy(item) for item in complete_list]
-    return await asyncio.gather(*tasks)
-
-
+# import checker
 # data = Scrapper().getData()
-# try:
-#     loop = asyncio.get_event_loop()
-#     # loop.run
-#     result = loop.run_until_complete(runner(data))
-#     print(result)
-# finally:
-#     loop.close()
-# return result
+# x = checker.Main(proxylist=data, checktype="socks5")
+# print("ddddddd")
 
-import checker
-data = Scrapper().getData()
-x = checker.Main(proxylist=data, checktype="socks5")
-print("ddddddd")
+
+# seq = ["This is 6th line", "This is 7th line"]
+
+Scrapper().init()
